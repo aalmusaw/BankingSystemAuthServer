7009614c78760e07f0139a1db43393c2dd5fdab3
Ali Al-Musawi <almuali699@gmail.com>
2020-12-27 13:25:14 -0500

    added db connection

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
 const express = require('express');
-const app = express();
-const PORT = 3000;
+const mongoose = require('mongoose');
 
-app.listen(PORT);
+// database connection string
+const uri = 'mongodb+srv://login:Hoho0IRu04Bc4Wgz@cluster0.kbjqe.mongodb.net/login?retryWrites=true&w=majority';
+
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true}, err=>{
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log('Successfully connected to database')
+    }
+});
+
+const app = express();
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`AuthServer is running on port ${PORT}`);
+});
