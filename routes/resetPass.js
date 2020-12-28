1399b34d2e54d881561381679d80c79e2978a856
Ali Al-Musawi <almuali699@gmail.com>
2020-12-28 12:09:59 -0500

    solved security breach with reset password

--- routes/resetPass.js
+++ routes/resetPass.js
@@ -13,29 +13,35 @@ router.post('/resetPass', (req, res, next) => {
     // extract email and user from user client's POST request
     const email = req.body.email;
     const password = req.body.password;
-    // encrypt the user-provided password
-    bycrypt.hash(password, 10, (err, encrypted) => {
-        // check for encryption error
-        if (err) {
-            res.status(500).json({message: 'Hashing error: password could not be reset'});
-        }
-        // no encryption error
-        else {
-            // update by email
-            const filter = {email: email};
-            // update old password to encrypted password
-            const update = {password: encrypted};
-            // execute query
-            User.findOneAndUpdate(filter, update, (err, doc, result)=> {
-                if (err) {
-                    res.status(500).json({message: 'Update error: password could not be reset'});
-                }
-                else {
-                    res.json({message: 'Password updated successfully'});
-                }
-            });
-        }
-    });
+    // check if the user is authorized
+    if (req.user.email != email) {
+        res.sendStatus(403);
+    }
+    else {
+        // encrypt the user-provided password
+        bycrypt.hash(password, 10, (err, encrypted) => {
+            // check for encryption error
+            if (err) {
+                res.status(500).json({ message: 'Hashing error: password could not be reset' });
+            }
+            // no encryption error
+            else {
+                // update by email
+                const filter = { email: email };
+                // update old password to encrypted password
+                const update = { password: encrypted };
+                // execute query
+                User.findOneAndUpdate(filter, update, (err, doc, result) => {
+                    if (err) {
+                        res.status(500).json({ message: 'Update error: password could not be reset' });
+                    }
+                    else {
+                        res.json({ message: 'Password updated successfully' });
+                    }
+                });
+            }
+        });
+    }
 });
 
 module.exports = router;
