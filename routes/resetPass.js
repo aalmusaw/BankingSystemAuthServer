885d474687513a500ac4e38ebebca970be980272
Ali Al-Musawi <almuali699@gmail.com>
2020-12-28 11:36:10 -0500

    implemented resetPass

---
+++ routes/resetPass.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+
+const bycrypt = require('bcrypt');
+const express = require('express');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/User');
+
+const router = express.Router();
+
+
+router.post('/resetPass', (req, res, next) => {
+    // extract email and user from user client's POST request
+    const email = req.body.email;
+    const password = req.body.password;
+    // encrypt the user-provided password
+    bycrypt.hash(password, 10, (err, encrypted) => {
+        // check for encryption error
+        if (err) {
+            res.status(500).json({message: 'Hashing error: password could not be reset'});
+        }
+        // no encryption error
+        else {
+            // update by email
+            const filter = {email: email};
+            // update old password to encrypted password
+            const update = {password: encrypted};
+            // execute query
+            User.findOneAndUpdate(filter, update, (err, doc, result)=> {
+                if (err) {
+                    res.status(500).json({message: 'Update error: password could not be reset'});
+                }
+                else {
+                    res.json({message: 'Password updated successfully'});
+                }
+            });
+        }
+    });
+});
+
+module.exports = router;
